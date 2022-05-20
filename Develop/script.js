f02554e1acc07c8ac1d614d5ed058c30dc371de4
Micah <micahculverhouse1@gmail.com>
2022-05-20 00:45:32 -0600

    finally got this local storage junk to work

--- Develop/script.js
+++ Develop/script.js
@@ -4,30 +4,31 @@ console.log(today);
 
 
 $(document).ready(function(){
+    
     $(".saveBtn").on("click", function() {
-        localStorage.setItem("myContent2", $("#hour-9").val());
-        localStorage.setItem("myContent3", $("#hour-10").val());
-        localStorage.setItem("myContent4", $("#hour-11").val());
-        localStorage.setItem("myContent5", $("#hour-12").val());
-        localStorage.setItem("myContent6", $("#hour-13").val());
-        localStorage.setItem("myContent7", $("#hour-14").val());
-        localStorage.setItem("myContent8", $("#hour-15").val());
-        localStorage.setItem("myContent9", $("#hour-16").val());
-        localStorage.setItem("myContent10", $("#hour-17").val());
+        
+        var textInput = $(this).siblings(".description").val();
+        var timeSection = $(this).parent().attr("id");
+        
+        localStorage.setItem(timeSection, textInput);
     })
+    
+    console.log(localStorage);
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 });
 
-function retrieveStorage(){
-    $("#hour-9 .description").val(localStorage.getItem("myContent2"));
-    $("#hour-10 .description").val(localStorage.getItem("myContent3"));
-    $("#hour-11 .description").val(localStorage.getItem("myContent4"));
-    $("#hour-12 .description").val(localStorage.getItem("myContent5"));
-    $("#hour-13 .description").val(localStorage.getItem("myContent6"));
-    $("#hour-14 .description").val(localStorage.getItem("myContent7"));
-    $("#hour-15 .description").val(localStorage.getItem("myContent8"));
-    $("#hour-16 .description").val(localStorage.getItem("myContent9"));
-    $("#hour-17 .description").val(localStorage.getItem("myContent10"));
-}
+
+    
+
 
 function timeBlockColors(){
     var currentTime = moment().hour();
@@ -43,13 +44,13 @@ function timeBlockColors(){
     }
     else if(timeBlock === currentTime){
         $(this).removeClass("past");
-        $(this).addClass("future");
-        $(this).removeClass("present");
+        $(this).addClass("present");
+        $(this).removeClass("future");
     }
     else{
         $(this).removeClass("past");
-        $(this).removeClass("future");
-        $(this).addClass("present");
+        $(this).removeClass("present");
+        $(this).addClass("future");
     
     }
 
