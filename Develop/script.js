245ba67de38a9e82fbb087289e04778552b1e969
Micah <micahculverhouse1@gmail.com>
2022-05-19 23:56:10 -0600

    still trying to fix local storage but got the tiles to change finally

--- Develop/script.js
+++ Develop/script.js
@@ -5,37 +5,33 @@ console.log(today);
 
 $(document).ready(function(){
     $(".saveBtn").on("click", function() {
-        localStorage.setItem("myContent", $(".9am").val());
-        localStorage.setItem("myContent", $(".10am").val());
-        localStorage.setItem("myContent", $(".11am").val());
-        localStorage.setItem("myContent", $(".12pm").val());
-        localStorage.setItem("myContent", $(".1pm").val());
-        localStorage.setItem("myContent", $(".2pm").val());
-        localStorage.setItem("myContent", $(".3pm").val());
-        localStorage.setItem("myContent", $(".4pm").val());
-        localStorage.setItem("myContent", $(".5pm").val());
-        console.log(localStorage.getItem($(".9am")));
+        localStorage.setItem("myContent2", $("#hour-9").val());
+        localStorage.setItem("myContent3", $("#hour-10").val());
+        localStorage.setItem("myContent4", $("#hour-11").val());
+        localStorage.setItem("myContent5", $("#hour-12").val());
+        localStorage.setItem("myContent6", $("#hour-13").val());
+        localStorage.setItem("myContent7", $("#hour-14").val());
+        localStorage.setItem("myContent8", $("#hour-15").val());
+        localStorage.setItem("myContent9", $("#hour-16").val());
+        localStorage.setItem("myContent10", $("#hour-17").val());
     })
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function retrieveStorage(){
+    $("#hour-9 .description").val(localStorage.getItem("myContent2"));
+    $("#hour-10 .description").val(localStorage.getItem("myContent3"));
+    $("#hour-11 .description").val(localStorage.getItem("myContent4"));
+    $("#hour-12 .description").val(localStorage.getItem("myContent5"));
+    $("#hour-13 .description").val(localStorage.getItem("myContent6"));
+    $("#hour-14 .description").val(localStorage.getItem("myContent7"));
+    $("#hour-15 .description").val(localStorage.getItem("myContent8"));
+    $("#hour-16 .description").val(localStorage.getItem("myContent9"));
+    $("#hour-17 .description").val(localStorage.getItem("myContent10"));
+}
 
 function timeBlockColors(){
     var currentTime = moment().hour();
-    console.log(currentTime)
+    console.log(currentTime);
 
     $('.time-block').each(function(){
     var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
@@ -53,7 +49,7 @@ function timeBlockColors(){
     else{
         $(this).removeClass("past");
         $(this).removeClass("future");
-        $(this).addClass("present")
+        $(this).addClass("present");
     
     }
 
